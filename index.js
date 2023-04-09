0570137ccbba68b1f12c747828ac4044f97dcafe
CodeMeYulia <yupipper@gmail.com>
2023-04-09 05:29:19 +0300

    add antispam

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const button = document.querySelector('.submit'); //переменная кнопки
+
 const photo = document.querySelector('.photo'); //переменная для поля ссылки
-let firstname = document.querySelector('.firstname'); //переменная для поля фио
-firstname = "Котовасий иванЫч Маодзедун";
-const mytext = document.querySelector('.mytext'); //переменная поля ввода сообщения в чате
 const avatar = document.querySelector('.avatar'); //переменная поля аватара в чате
-const nickname = document.querySelector('.nickname'); //переменная поля имени в чате
+
+const mytext = document.querySelector('.mytext'); //переменная поля ввода сообщения в чате
 const message = document.querySelector('.message'); //переменная строчки сообщения в чате
 
-const arr = firstname.split(" "); //получение массива строки имени
-console.log(arr);
+let firstname = document.querySelector('.firstname'); //переменная для поля фио
+const nickname = document.querySelector('.nickname'); //переменная поля имени в чате
 
-const newArr = []; //компиляции нового массива строки имени
+
+// alert(mytext);
+// let antispam = mytext.replace(/viagra/gi, '*****');
+
+firstname = "Котовасий иванЫч Маодзедун";
+
+
+const arr = firstname.split(" "); //получение массива строки имени
+// console.log(arr);
+const ArrUpp = []; //компиляции нового массива строки имени
 
 for (let i=0; i < arr.length; i++) {
-    newArr.push(arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1).toLowerCase()); //преобразование элементов массива строки имени
+    ArrUpp.push(arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1).toLowerCase()); //преобразование элементов массива строки имени
 };
+let firstnameUp = ArrUpp.join(' '); //объединение элементов массива строки имени
+// alert(firstnameUp);
 
-let firstnameUp = newArr.join(' '); //объединение элементов массива строки имени
-console.log(firstnameUp);
 
 button.addEventListener('click', () => {  //отправка сообщения в чат по нажатию кнопки в форме
     avatar.innerHTML = `<img src = ${photo.value} alt="userpic">`;
     nickname.textContent = `${firstnameUp}: `;
-    message.textContent = `"${mytext.value}"`;
+    message.textContent = (`"${mytext.value}"`).replace(/viAgrA/gi, "*****").replace(/xxx/gi, "*****"); //запись переданного сообщения в чат С АНТИСПАМОМ
+    
+    
 });
-
-
-// Котовасий иванЫч Маодзедун
 
